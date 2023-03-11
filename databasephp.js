890aa2e0933aa177ac2173430d4f40a4d9ee20ba
Hoang Vy <19161199@student.hcmute.edu.vn>
2023-03-11 17:16:00 +0700

    real nodejs

--- databasephp.js
+++ databasephp.js
@@ -1,9 +1,15 @@
 const mysql = require("mysql");
 require('dotenv').config();
+
+host = process.env.DB_HOST;
+user= process.env.DB_USER;
+password=process.env.DB_PASSWORD;
+database=process.env.DB_DATABASE;
+
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: host,
+    user: user,
+    password: password,
     database: process.env.DB_DATABASE
 })
 module.exports = db;
