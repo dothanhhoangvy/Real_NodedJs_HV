a5d7de3ffab96a7a4917a4e2e908c74b55e719e8
Hoang Vy <19161199@student.hcmute.edu.vn>
2023-06-02 20:46:04 +0700

    first commit

--- routes/data.js
+++ routes/data.js
@@ -21,7 +21,8 @@ router.route("/fuel").get((req, res) => {
       console.log(err);
     } else {
       var data1 = JSON.parse(JSON.stringify(data));
-      return res.json(data1);
+      data2 = data1.slice(0, 50);
+      return res.json(data2);
     }
   });
 });
@@ -32,7 +33,8 @@ router.route("/engtmp").get((req, res) => {
       console.log(err);
     } else {
       var data1 = JSON.parse(JSON.stringify(data));
-      return res.json(data1);
+      data2 = data1.slice(0, 50);
+      return res.json(data2);
     }
   });
 });
@@ -43,7 +45,8 @@ router.route("/speed").get((req, res) => {
       console.log(err);
     } else {
       var data1 = JSON.parse(JSON.stringify(data));
-      return res.json(data1);
+      data2 = data1.slice(0, 50);
+      return res.json(data2);
     }
   });
 });
